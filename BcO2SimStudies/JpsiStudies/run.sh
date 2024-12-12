2354da0de64a7e56f9e8e93588ab99d30277efad
Paul Veen <paul.veen@yahoo.com>
2024-12-12 16:36:47 +0100

    Update run.sh

--- BcO2SimStudies/JpsiStudies/run.sh
+++ BcO2SimStudies/JpsiStudies/run.sh
@@ -1,4 +1,4 @@
 set -x
 
 NEV=100
-o2-sim -n ${NEV} -g pythia8 --configKeyValues "GeneratorPythia8.config=decay_jpsi.cfg"
+o2-sim -j 20 -n ${NEV} -g pythia8 --configKeyValues 'SimUserDecay.pdglist=443;GeneratorPythia8.config=decay_jpsi.cfg;DecayerPythia8.verbose=true'
