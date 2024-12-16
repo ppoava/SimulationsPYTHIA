4fb95b3c992aa3f2a03d9a3a374284c4b8419125
Paul Veen <paul.veen@yahoo.com>
2024-12-16 09:29:01 +0100

    Update readKinematics.C

--- BcO2SimStudies/readKinematics.C
+++ BcO2SimStudies/readKinematics.C
@@ -50,75 +50,98 @@ int readKinematics() {
   Int_t nJpsi = 0;
   Int_t nMuon = 0;
   Int_t nAntiMuon = 0;
+  Int_t particlePdgCode,motherTrackId,motherPdgCode,triggerMotherTrackId,associatePdgCode,associateMotherTrackId,associateMotherPdgCode;
+  Double_t pT,eta,vX,vY,vZ;
 
 
-  TFile inputKine("JpsiStudies/o2sim_Kine.root","READ");
-    auto tree = (TTree*)inputKine.Get("o2sim");
-    std::vector<o2::MCTrack>* tracks{};
-    tree->SetBranchAddress("MCTrack", &tracks);
+  TFile inputKine("JpsiStudies/1e2_o2sim_Kine.root","READ");
+  auto tree = (TTree*)inputKine.Get("o2sim");
+  std::vector<o2::MCTrack>* tracks{};
+  tree->SetBranchAddress("MCTrack", &tracks);
 
 
-    // TO DO 
-    // ADD RANDOM COMBINATIONS OF MUONS (OR SEMI-RANDOM) OR AT LEAST MOTIVATED BY EXPERIMENT 
-    // (e.g. similar origin vertex, etc.)
+  // TO DO 
+  // ADD RANDOM COMBINATIONS OF MUONS (OR SEMI-RANDOM) OR AT LEAST MOTIVATED BY EXPERIMENT 
+  // (e.g. similar origin vertex, etc.)
 
-    for (int ev = 0; ev < tree->GetEntries(); ++ev) {
+  for (int ev = 0; ev < tree->GetEntries(); ++ev) {
 
 
-        tree->GetEntry(ev);
-        std::cout<<"NEXT EVENT"<<std::endl<<std::endl;
+      tree->GetEntry(ev);
+      std::cout<<"NEXT EVENT"<<std::endl<<std::endl;
 
-        for (auto& track : *tracks) {
 
-          // if (!isFinalState(&track)) { continue; }
+      for (auto& track : *tracks) {
 
-          hID->Fill(track.GetPdgCode());
-          Int_t particlePdgCode = track.GetPdgCode();
-          Int_t motherTrackId = track.getMotherTrackId();
-          const o2::MCTrack& motherTrack = (*tracks)[motherTrackId];
-          Int_t motherPdgCode = motherTrack.GetPdgCode();
-          
-          // std::cout<<"pdg code = "<<particlePdgCode<<std::endl;
 
-          if (particlePdgCode == 443) { // Jpsi
-            nJpsi++;
-            std::cout<<"me = "<<particlePdgCode<<std::endl;
-            std::cout<<"my mom = "<<motherPdgCode<<std::endl<<std::endl;
-          }
+        // if (!isFinalState(&track)) { continue; }
 
-          if (particlePdgCode == 13) { // mu-
-            nMuon++;
-            TLorentzVector trigger4P;
-            track.Get4Momentum(trigger4P);
-            Int_t triggerMother = track.getMotherTrackId();
-            std::cout<<"trigger mother = "<<triggerMother<<std::endl;
-            std::cout<<"me = "<<particlePdgCode<<std::endl;
-            std::cout<<"my mom = "<<motherPdgCode<<std::endl<<std::endl;
-            for (auto& track : *tracks) { // couple with mu+ now
-              Int_t associatePdgCode = track.GetPdgCode();
-              Int_t associateMother = track.getMotherTrackId();
-              // std::cout<<"associateMother = "<<associateMother<<std::endl;
-              if (associatePdgCode == -13 && associateMother == triggerMother) { // mu+ found 
-                nAntiMuon++;
-                TLorentzVector associate4P;
-                track.Get4Momentum(associate4P);
-                TLorentzVector dimuon4P = trigger4P+associate4P;
-                Double_t dimuonM = dimuon4P.M();
-                std::cout<<"Di-muon mass = "<<dimuonM<<std::endl;
-                hDiMuonMass->Fill(dimuonM);
-              }
+        pT = track.GetPt();
+        eta = track.GetEta();
+        vX = track.GetStartVertexCoordinatesX();
+        vY = track.GetStartVertexCoordinatesY();
+        vZ = track.GetStartVertexCoordinatesZ();
+
+        // if (eta < -3.6 || eta > -2.5) { continue; } // MFT pseudorapidity cut
+
+        hID->Fill(track.GetPdgCode());
+        particlePdgCode = track.GetPdgCode();
+        motherTrackId = track.getMotherTrackId();
+        const o2::MCTrack& motherTrack = (*tracks)[motherTrackId];
+        motherPdgCode = motherTrack.GetPdgCode();
+        
+        // std::cout<<"pdg code = "<<particlePdgCode<<std::endl;
+
+        if (particlePdgCode == 443) { // Jpsi (use code == 2212 (proton) for non-prompt/prompt distinction)
+          nJpsi++;
+          // std::cout<<"me = "<<particlePdgCode<<std::endl;
+          // std::cout<<"my mom = "<<motherPdgCode<<std::endl<<std::endl;
+        }
+
+        // NOTE: seems like there is misID effects, sometimes coupling gives e.g. an eta->mumu
+        if (particlePdgCode == 13) { // mu-
+          nMuon++;
+          TLorentzVector trigger4P;
+          track.Get4Momentum(trigger4P);
+          // std::cout<<"me = "<<particlePdgCode<<std::endl;
+          // std::cout<<"my mom = "<<motherPdgCode<<std::endl<<std::endl;
+          for (auto& track : *tracks) { // couple with mu+ now
+            associatePdgCode = track.GetPdgCode();
+            associateMotherTrackId = track.getMotherTrackId();
+            const o2::MCTrack& associateMotherTrack = (*tracks)[associateMotherTrackId];
+            associateMotherPdgCode = motherTrack.GetPdgCode();
+            // std::cout<<"associateMother = "<<associateMother<<std::endl;
+            if (associatePdgCode == -13 && associateMotherTrackId == motherTrackId) { // mu+ and same mother as mu-
+              nAntiMuon++;
+              TLorentzVector associate4P;
+              std::cout<<"me = "<<associatePdgCode<<std::endl;
+              std::cout<<"my mom = "<<associateMotherPdgCode<<std::endl;
+              std::cout<<"and my trigger mom = "<<motherPdgCode<<std::endl<<std::endl;
+              track.Get4Momentum(associate4P);
+              TLorentzVector dimuon4P = trigger4P+associate4P;
+              Double_t dimuonM = dimuon4P.M();
+              std::cout<<"Di-muon mass = "<<dimuonM<<std::endl;
+              hDiMuonMass->Fill(dimuonM);
             }
           }
-    
+        } // end of di-muon coupling
 
-        }
-    }
-    std::cout<<"Number of jpsi found = "<<nJpsi<<std::endl;
-    std::cout<<"Number of muons found = "<<nMuon<<std::endl;
-    std::cout<<"Number of anti-muons found = "<<nAntiMuon<<std::endl;
-    TFile outputFile("JpsiStudies/output.root", "RECREATE");
-    hID->Write();
-    hDiMuonMass->Write();
 
-    return 0;
+      } // end of tracks
+
+
+    } // end of event
+
+
+  std::cout<<"Number of jpsi found = "<<nJpsi<<std::endl;
+  std::cout<<"Number of muons found = "<<nMuon<<std::endl;
+  std::cout<<"Number of di-muons found = "<<nAntiMuon<<std::endl;
+  TFile outputFile("JpsiStudies/output.root", "RECREATE");
+  hID->Write();
+  hDiMuonMass->Write();
+
+
+  return 0;
+
+
 }
