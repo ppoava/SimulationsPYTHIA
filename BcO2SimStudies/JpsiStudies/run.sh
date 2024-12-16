168e4f294219f32d86319df197f70a132a4a635f
Paul Veen <paul.veen@yahoo.com>
2024-12-16 10:15:05 +0100

    Update run.sh

--- BcO2SimStudies/JpsiStudies/run.sh
+++ BcO2SimStudies/JpsiStudies/run.sh
@@ -1,4 +1,10 @@
 set -x
 
 NEV=1000
-o2-sim -j 20 -n ${NEV} -g pythia8 --configKeyValues 'SimUserDecay.pdglist=443;GeneratorPythia8.config=decay_jpsi.cfg;DecayerPythia8.verbose=true'
+NODES=10
+
+### "minimum bias with jpsi->mumu forced"
+### o2-sim -j ${NODES} -n ${NEV} -g pythia8 --configKeyValues "SimUserDecay.pdglist=443;GeneratorPythia8.config=decay_jpsi.cfg;DecayerPythia8.verbose=true"
+
+### boxgen insertion 1 jpsi per event, jpsi->mumu forced
+o2-sim -j ${NODES} -n ${NEV} -g boxgen --configKeyValues "BoxGun.pdg=443;BoxGun.number=1;SimUserDecay.pdglist=443;DecayerPythia8.config[1]=decay_jpsi.cfg;DecayerPythia8.verbose=true"
