96b773215e6df63e20762f3d0be08570ffcf13c8
Paul Veen <paul.veen@yahoo.com>
2024-12-19 21:36:33 +0100

    Update run.sh

--- BcO2SimStudies/JpsiStudies/run.sh
+++ BcO2SimStudies/JpsiStudies/run.sh
@@ -4,7 +4,7 @@ NEV=10
 NODES=10
 
 ### "minimum bias with jpsi->mumu forced"
-# o2-sim -j ${NODES} -n ${NEV} -g pythia8 --configKeyValues "SimUserDecay.pdglist=443;GeneratorPythia8.config=decay_jpsi.cfg;DecayerPythia8.verbose=true"
+o2-sim -j ${NODES} -n ${NEV} -g pythia8 --configKeyValues "SimUserDecay.pdglist=443;GeneratorPythia8.config=decay_jpsi.cfg;DecayerPythia8.verbose=true"
 
 ### "minimum bias with bc+->mu nu jpsi->mumu forced"
 # doesn't work???
